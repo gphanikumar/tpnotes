448911259abc9a1c13120bf8013e8dc7df2d7adf
Head CC IITM <cchead@iitm.ac.in>
2025-02-02 11:54:45 +0530

    added source files

---
+++ source/clean.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+# script to clean up temporary files after latex
+# -------------------------------------------------------
+echo "Cleaning up..."
+arg=${1:-.}
+exts="aux bbl blg brf idx ilg ind loe lof log lol lot out toc synctex.gz"
+
+if [ -d $arg ]; then
+    for ext in $exts; do
+         rm -f $arg/*.$ext
+    done
+else
+    for ext in $exts; do
+         rm -f $arg.$ext
+	done
+fi
+# end of clean.sh
